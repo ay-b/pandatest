2a3a8ef42df9870cf4a512c09e09e04155e780f4
Alexander Yankovskiy-Betcher <a@y-b.one>
2018-12-27 23:44:06 +0200

    Initital commit

---
+++ deploy.py
@@ -0,0 +1,101 @@
+import os
+import shutil
+import requests
+
+
+def check_deploy_folder_exist(stuff_path, clean=0):
+    if os.path.isdir(stuff_path) and clean == 1:
+        print("Deploy folder already exists. Deleting...")
+        remove_deploy_folder(stuff_path)
+    elif os.path.isdir(stuff_path) and clean != 1:
+        print("Deployment folder in place")
+    else:
+        print("No deployment folder detected.")
+
+
+def remove_deploy_folder(stuff_path):
+    print("Removing folder: " + stuff_path)
+    shutil.rmtree(stuff_path)
+    if os.path.isdir(stuff_path):
+        print("ERROR: Failed. Exiting.")
+        exit(1)
+
+
+def create_deploy_folder(stuff_path):
+    clean = 1
+    check_deploy_folder_exist(stuff_path, clean)
+    os.mkdir(stuff_path, 0o755);
+    check_deploy_folder_exist(stuff_path)
+
+
+def get_git_repo(repo, stuff_path):
+    cmd = "git clone " + repo + " " + stuff_path
+    exit_code = os.system(cmd)
+    if exit_code == 0:
+        print("Sources obtained")
+    else:
+        print("ERROR: Failed to obtain sources. Exiting.")
+        exit(1)
+
+
+def create_images_folder(stuff_path):
+    print("Creating images folder")
+    os.mkdir(stuff_path + "/public/images")
+    if os.path.isdir(stuff_path + "/public/images"):
+        print("Images folder in place")
+    else:
+        print("ERROR: Error creating images folder")
+        exit(1)
+
+
+
+def check_content_type(content_url, content_type):
+    print("Checking content type")
+    res = requests.get(content_url)
+    content_type = res.headers['content-type']
+    if content_type == "application/x-gzip":
+        print("Found zipped content. Proceeding.")
+    else:
+        print("ERROR: Unexpected thing at URL.")
+        exit(1)
+    return content_type
+
+
+def unzip_content(content_folder, filename):
+    cmd = "tar -xzvf " + content_folder + "/" + filename + " -C " + content_folder
+    exit_code = os.system(cmd)
+    if exit_code == 0:
+        print("Unzipped successfully.")
+    else:
+        print("ERROR: Unzipping failed. Exiting.")
+        exit(1)
+    cmd = "rm " + content_folder + "/" + filename
+    os.system(cmd)
+
+def download_content(content_url, content_folder, filename):
+    content_type = "error"
+    print("Downloading content bundle.")
+    check_content_type(content_url, content_type)
+    bundle = requests.get(content_url, allow_redirects=True)
+    with open(content_folder + "/" + filename, "wb") as r:
+        r.write(bundle.content)
+    print("Finished downloading.")
+    return filename
+
+
+def main():
+    stuff_path = "./temp_deploy"
+    repo = "https://github.com/bigpandaio/ops-exercise"
+    content_url = "https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz"
+    content_folder = stuff_path + "/public/images"
+    filename = content_url.rsplit('/', 1)[1]
+
+    create_deploy_folder(stuff_path)
+    get_git_repo(repo, stuff_path)
+    create_images_folder(stuff_path)
+    download_content(content_url, content_folder, filename)
+    unzip_content(content_folder,filename)
+
+
+if __name__ == "__main__":
+    main()
